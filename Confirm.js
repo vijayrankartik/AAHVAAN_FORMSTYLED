63d6241173a286d34a0bec22a952c8babd3a241c
vijayrankartik <58977367+vijayrankartik@users.noreply.github.com>
2019-12-29 14:45:31 +0530

    Update Confirm.js

--- Confirm.js
+++ Confirm.js
@@ -1,47 +1,68 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
 
 const useStyles = makeStyles(theme => ({
-  
-  
-}
+  root: {
+    background: 'white',
+    color: 'black',
+  },
+  p: {
+    margin: 5,
+    padding: 5
+  }
+}));
 
-)
-  )
+
 
 const generateMembersList = (members) => {
   return(
-    members.map((member, index) => <p>Member {index + 1}: {member}</p>)
+    members.map((member, index) => <p useStyles>Member {index + 1}: {member}</p>)
   )
 }
 
 const Confirm = ({ data }) => {
+  const classes = useStyles();
   if (data.single) {
     // For single registrations.
     return (
-      <React.Fragment>
-        <p>Registration type: Single</p>
-        <p>Name: {data.name}</p>
-        <p>Email-id: {data.email}</p>
-        <p>Event: {data.events}</p>
-        <p>College: {data.college}</p>
-        <p>Accommodation wanted: {data.stay ? 'Yes' : 'No'}</p>
+      <div align='center'>
+      <React.Fragment className={classes.root}>
+          <h1 align="center">
+          <AppBar color="secondary">
+            CONFIRM
+          </AppBar> 
+        </h1>
+        <p className={classes.p}>Registration type: Single</p>
+        <p className={classes.p}>Name: {data.name}</p>
+        <p className={classes.p}>Email-id: {data.email}</p>
+        <p className={classes.p}>Event: {data.events}</p>
+        <p className={classes.p}>College: {data.college}</p>
+        <p className={classes.p}>Accommodation wanted: {data.stay ? 'Yes' : 'No'}</p>
       </React.Fragment>
+      </div>
     )
   } else {
     // For team registrations.
     return (
+      <div align='center'>
       <React.Fragment>
-        <p>Registration type: Team</p>
-        <p>Name (Leader): {data.name}</p>
-        <p>Email-id (Leader): {data.email}</p>
-        <p>Event: {data.events}</p>
-        <p>Members Count: {data.teamMemberCount}</p>
-        <p>Members Name:</p>
+          <h1 align="center">
+          <AppBar color="secondary">
+            CONFIRM
+          </AppBar> 
+        </h1>
+        <p className={classes.p}>Registration type: Team</p>
+        <p className={classes.p}>Name (Leader): {data.name}</p>
+        <p className={classes.p}>Email-id (Leader): {data.email}</p>
+        <p className={classes.p}>Event: {data.events}</p>
+        <p className={classes.p}>Members Count: {data.teamMemberCount}</p>
+        <p className={classes.p}>Members Name:</p>
         {generateMembersList(data.teamMemberName)}
-        <p>College: {data.college}</p>
-        <p>Accommodation wanted: {data.stay ? 'Yes' : 'No'}</p>
+        <p className={classes.p}>College: {data.college}</p>
+        <p className={classes.p}>Accommodation wanted: {data.stay ? 'Yes' : 'No'}</p>
       </React.Fragment>
+      </div>
     )
   }
 }
