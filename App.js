4ae90590a6fe266ddfb32de6b6522d7d1db5f489
vijayrankartik <58977367+vijayrankartik@users.noreply.github.com>
2019-12-29 14:44:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,21 +4,17 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import {Gallery, GalleryImage } from 'react-gesture-gallery';
 import { createMuiTheme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import { ThemeProvider } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import zIndex from '@material-ui/core/styles/zIndex';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faUsers, faChevronCircleDown} from '@fortawesome/free-solid-svg-icons'
+import { faUser, faUsers, faChevronCircleDown, faArrowUp} from '@fortawesome/free-solid-svg-icons'
 import { faAdobe, faApple, faGoogle, faAmazon, faMicrosoft } from '@fortawesome/free-brands-svg-icons'
-
+import Logo from './_img/logo.png'
 
 // import the custon CSS
 import './styles/index.css'
@@ -50,6 +46,44 @@ const theme = createMuiTheme({
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+  },
+  avatar: {
+    color: 'white',
+    backgroundColor: 'gray',
+    border: '4px solid black',
+    padding: '12px',
+    width: '50px',
+    height: '50px',
+    borderRadius: '50%',
+    marginTop: '15px',
+    marginBottom: '14px',
+  },
+  navBar: {
+    padding: '13px',
+    backgroundColor: 'black',
+    opacity: '0.76',
+    height: '10vh',
+    zIndex: '1',
+  },
+  navButtonContact: {
+    position: 'absolute',
+    top: '2.5vh',
+    minTop: '10px',
+    right: '18px',
+    color: 'gold',
+    opacity: '1',
+    fontSize: '20px',
+    zIndex: '2',
+  },
+  navButtonAbout: {
+    position: 'absolute',
+    top: '2.5vh',
+    minTop: '10px',
+    right: '183px',
+    color: 'gold',
+    opacity: '1',
+    fontSize: '20px',
+    zIndex: '2',
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -117,7 +151,26 @@ const useStyles = makeStyles(theme => ({
     opcaity:1,
     borderWidth: 'thick ',
     zIndex: 2,
-  }
+  },
+  footer: {
+    
+  },
+  upper: {
+    backgroundColor: 'gray',
+    color: 'white',
+    padding: '10px',
+    display: 'flex',
+    alignItems: 'stretch',
+  },
+  lower: {
+    display: 'flex',
+    color: 'white',
+    backgroundColor: 'black',
+    paddingBottom: '13px',
+  },
+  buttonUp: {
+    color: 'gold',
+  },
 }));
 
 export default function ButtonAppBar() {
@@ -142,15 +195,21 @@ export default function ButtonAppBar() {
 
   return (
     <div className="app">
-      <div className={classes.root}>
-        <AppBar position="sticky" color='secondary'>
-          <Toolbar>
-            <Typography variant="h5" className={classes.title} align='center'>
-              AAHVAAN
-            </Typography>
-          </Toolbar>
+
+      {/*---------Navbar----------------*/}
+      <div>
+        <AppBar className={classes.navBar} position="sticky" color="secondary">
         </AppBar>
       </div>
+      <div id="navbar-content">
+        <Typography className={classes.root}>
+          <Button className={classes.navButtonAbout} href="">About</Button>
+          <Button className={classes.navButtonContact} href="">Contact Us</Button>
+        </Typography>
+      </div>
+      {/*-------------------------------*/}
+
+      {/*----------Overlay--------------*/}
       <div className="overlay-text-container" align="center">
         <h1 className="overlay-text">
           AAHVAAN
@@ -175,6 +234,38 @@ export default function ButtonAppBar() {
           ))}
         </Gallery>
       </Paper>
+      {/*-------------------------------*/}
+
+      {/*-----------About section-------------*/}
+      <div id="about">
+        <Paper>
+          <Card>
+            <AppBar position="sticky" color='secondary'>
+              <Toolbar>
+                <Typography variant="h5" className={classes.title} align='center'>
+                  ABOUT
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at convallis dui, hendrerit 
+              pharetra justo. Duis maximus, dui vitae suscipit lobortis, ipsum diam ultrices quam, vel suscipit 
+              ipsum nibh in orci. Curabitur vitae augue eleifend tortor varius varius. Etiam non elementum leo. 
+              Phasellus id metus non est mattis ultrices. Praesent vel metus eget velit mollis dictum ac sed nunc. 
+              Phasellus mattis, orci sed dictum malesuada, nulla nulla fringilla mauris, at convallis lacus 
+              metus vel risus. In sagittis porttitor pharetra.
+              Morbi ut pulvinar metus. Suspendisse quis risus vel ligula dictum volutpat et at dui. Nulla 
+              vestibulum dapibus turpis eget feugiat. Suspendisse erat nibh, blandit eget sodales a, 
+              fermentum a velit. Nulla consequat iaculis enim eget tincidunt. Pellentesque sapien ipsum, 
+              ultricies sodales est non, sollicitudin malesuada erat. Nunc vulputate tempus venenatis. 
+              Phasellus tincidunt dignissim enim vitae pretium. Ut tristique, massa a tristique viverra, 
+              purus nulla pretium metus, et mattis nisl magna ac lorem. Donec et gravida dolor. Phasellus 
+              facilisis risus nec magna vestibulum, et auctor orci maximus.
+            </div>
+          </Card>
+        </Paper>
+      </div>
+      {/*-------------------------------------*/}
 
       {/*---------Form Components-------------*/}
       <div className={classes.root}>
@@ -192,15 +283,21 @@ export default function ButtonAppBar() {
       <Grid container spacing={3}>
         <Grid item xs={6}>
           <Paper className={classes.paper} align="center">
+            <div className={classes.avatar}>
+              <FontAwesomeIcon icon={faUser} />
+            </div>
             <Button component={ Link } to="/single" variant="contained" color="secondary">
-              <FontAwesomeIcon icon={faUser} />&nbsp;Single
+              Single
             </Button>
           </Paper>
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper} align="center">
+            <div className={classes.avatar}>
+              <FontAwesomeIcon icon={faUsers} />
+            </div>
             <Button component={ Link } to="/team" variant="contained" color="secondary">
-              <FontAwesomeIcon icon={faUsers} />&nbsp;Team
+              Team
             </Button>
           </Paper>
         </Grid>
@@ -214,36 +311,43 @@ export default function ButtonAppBar() {
             Our Sponsers
           </Typography>
           <Typography variant="h6" align="center">
-            <FontAwesomeIcon icon={faAdobe} />&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faAmazon} />&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faApple} />&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faGoogle} />&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faMicrosoft} />&nbsp;&nbsp;
+            <div className="sponsors">
+              <FontAwesomeIcon icon={faAdobe} />&nbsp;&nbsp;
+              <FontAwesomeIcon icon={faAmazon} />&nbsp;&nbsp;
+              <FontAwesomeIcon icon={faApple} />&nbsp;&nbsp;
+              <FontAwesomeIcon icon={faGoogle} />&nbsp;&nbsp;
+              <FontAwesomeIcon icon={faMicrosoft} />&nbsp;&nbsp;
+            </div>
           </Typography>
         </Card>
       </Paper>
       {/*------------------------------------*/}
 
-      <Paper align="center">
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
-              Delhi Technological University
-            </Typography>
-            <Typography variant="h5" component="h2" color="textSecondary">
-              AAHVAAN
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              @aahvaan
-            </Typography>
-            <Typography variant="body2" component="p" color="textSecondary">
-              THIS IS FOOTER
+      {/*------------Footer------------------*/}
+      <Paper>
+        <Card className={classes.footer}>
+          <div className={classes.upper} align="right">
+            <Typography style={{width: '100px',}}>For help or any queries:</Typography>
+            <p style={{width: 'calc(100vw - 320px'}}></p>
+            <Typography style={{width: '220px',}}>
+              Contact-1: 8373-773747
               <br />
-              {'"sports spirit"'}
+              Contact-2: 8373-773747
             </Typography>
-          </CardContent>
+          </div>
+          
+          <div className={classes.lower}>
+            <img src={Logo} width="50px" height="60px" alt="Aahvaan Logo" />
+            <p style={{width: 'calc(100vw - 100px)', textAlign: 'center', paddingTop: '10px'}}> 
+              @Copyright 2020 AAHVAAN DTU
+            </p>
+            <Button href="" className={classes.buttonUp} style={{width: '50px', fontSize: '20px'}}>
+              <FontAwesomeIcon icon={faArrowUp} />
+            </Button>
+          </div>
         </Card>  
       </Paper>
+      {/*------------------------------------*/}
     </div>    
   );
 }
