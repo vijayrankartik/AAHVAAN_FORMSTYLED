28ac00c8d872b71e5f1c665f96f32ce647038cd8
vijayrankartik <58977367+vijayrankartik@users.noreply.github.com>
2019-12-31 13:17:31 +0530

    Update Confirm.js

--- Confirm.js
+++ Confirm.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Box from '@material-ui/core/Box'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -27,12 +30,17 @@ const Confirm = ({ data }) => {
     // For single registrations.
     return (
       <div align='center'>
-      <React.Fragment className={classes.root}>
-          <h1 align="center">
+                <h1 align="center">
           <AppBar color="secondary">
             CONFIRM
           </AppBar> 
         </h1>
+        <br />
+      <br /><br /><br />
+      <Card className={classes.Card} variant='outlined'>
+      <CardContent>
+      <Box boxShadow={5}>
+      <React.Fragment className={classes.root}>
         <p className={classes.p}>Registration type: Single</p>
         <p className={classes.p}>Name: {data.name}</p>
         <p className={classes.p}>Email-id: {data.email}</p>
@@ -40,18 +48,25 @@ const Confirm = ({ data }) => {
         <p className={classes.p}>College: {data.college}</p>
         <p className={classes.p}>Accommodation wanted: {data.stay ? 'Yes' : 'No'}</p>
       </React.Fragment>
+      </Box>
+      </CardContent>
+      </Card>
       </div>
     )
   } else {
     // For team registrations.
     return (
       <div align='center'>
-      <React.Fragment>
-          <h1 align="center">
+                <h1 align="center">
           <AppBar color="secondary">
             CONFIRM
-          </AppBar> 
-        </h1>
+          </AppBar>
+          </h1>
+          <br /><br /><br /><br />
+          <Card className={classes.Card} variant='outlined'>
+      <CardContent>
+      <Box boxShadow={5}>    
+      <React.Fragment>
         <p className={classes.p}>Registration type: Team</p>
         <p className={classes.p}>Name (Leader): {data.name}</p>
         <p className={classes.p}>Email-id (Leader): {data.email}</p>
@@ -62,6 +77,9 @@ const Confirm = ({ data }) => {
         <p className={classes.p}>College: {data.college}</p>
         <p className={classes.p}>Accommodation wanted: {data.stay ? 'Yes' : 'No'}</p>
       </React.Fragment>
+      </Box>
+      </CardContent>
+      </Card>
       </div>
     )
   }
